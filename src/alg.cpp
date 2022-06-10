c3a985f86a3d8065aa94adb1d559564bba50746c
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 17:45:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> Drevo;
+  std::ifstream fin(filename);
+  char tempr;
+  std::string word = "";
+  if (!fin.is_open()) {
+      std::cout << "Error! File can't be open!" << std::endl;
+      return Drevo;
+  }
+  while (!fin.eof()) {
+      tempr = fin.get();
+      if (tempr >= 'A') {
+          if (tempr <= 'Z') {
+              tempr = tempr + 32;
+          }
+      }
+      if (tempr >= 'a') {
+          if (tempr <= 'z') {
+              word = word + tempr;
+          }
+      } else {
+          Drevo.Add(word);
+          word = "";
+      }
+  }
+  fin.close();
+  return Drevo;
 }
